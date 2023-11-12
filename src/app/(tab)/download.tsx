e805609be76cb009990a84f425ea0fc476ec0fa7
Lucas Fernandes Silva <lucasdotfs@gmail.com>
2023-11-11 22:55:04 -0300

    zip complete
    Eu perdi a batalha mas não a guerra. Passei dois dias tentando fazer o zip funcionar e finalmente consegui. Sinceramente, não sei o que fiz para funcionar. Continuo usando o expo. Por incrivel que pareça estou mais motivado.

--- src/app/(tab)/download.tsx
+++ src/app/(tab)/download.tsx
@@ -1,14 +1,29 @@
 import Layout from "@/components/Layout"
 import Colors from "@/constants/Colors"
 import useGameDownload from "@/hooks/useGameDownload"
-import { StyleSheet, Button } from "react-native"
+import { StyleSheet, Button, Image } from "react-native"
+import * as FileSystem from "expo-file-system"
+import { useState } from "react"
 
 export default function Download() {
     useGameDownload()
-    
+    const [uri, setUri] = useState<any>()
 
+  
+    const path = FileSystem.cacheDirectory + "unzipped/2015_3_meio_ambiente/1.jpg"
+
+    async function handleImage() {
+        const info = await FileSystem.getInfoAsync(path);
+        console.log(info)
+        if(info.exists) {
+            console.log("ss")
+            setUri(() => info.uri)
+        }
+    }
+    handleImage()
     return (
         <Layout title="Acervo Online" >
+            <Image style={{ width: 100, height: 100 }} source={{ uri }} />
             <Button  title="Download" />
         </Layout>
     )
--- src/hooks/useGameDownload.ts
+++ src/hooks/useGameDownload.ts
@@ -4,23 +4,9 @@ import { unzip } from 'react-native-zip-archive';
 
 
 export default async function useGameDownload() {
-    const url = 'http://intervox.nce.ufrj.br/~projetojogavox/Site_Jogavox/Jogos/Olimpo/2015_3_meio_ambiente.zip';
-    const path = FileSystem.cacheDirectory + '1.zip'
-    console.log(path)
-    const downloadResumable = FileSystem.createDownloadResumable(
-        url,
-        path,
-    );
-        
-    await downloadResumable.downloadAsync();
-    unzip(path, FileSystem.cacheDirectory + "unzipped/")
-        .then(() => {
-            console.log('Unzip completed!');
-        })
-        .catch((error) => {
-            console.error('Unzip failed: ', error);
-        });
+    const files = FileSystem.readDirectoryAsync(FileSystem.cacheDirectory + "unzipped/2015_3_meio_ambiente")
     
+
     
 }
 
